c63975198112c2e7c4a1fef622df569e392b8d03
neilellis <hello@neilellis.me>
2015-07-07 18:21:57 +0100

    Bug fixing

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -x
 env_vars=$(env | grep ".*_TUTUM_API_URL=" | cut -d= -f1 | tr '\n' ' ')
 for env_var in $env_vars
 do
-  echo "*/5 * * * * curl -X POST -H \"Authorization: $TUTUM_AUTH\" -H \"Accept: application/json\" $env_var/start/ &> /dev/stdout" > /tmp/cron.tmp
+  echo "*/5 * * * * curl -X POST -H \"Authorization: $TUTUM_AUTH\" -H \"Accept: application/json\" ${!env_var}/start/ &> /dev/stdout" > /tmp/cron.tmp
 done
 
 echo "Installing new crontab"
