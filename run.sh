0d4eb46c81937a022a81ac80f886fb76c8f3f865
Neil Ellis <hello@neilellis.me>
2015-07-07 17:39:24 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
- curl -H "Authorization: $TUTUM_AUTH" -H "Accept: application/json" $TUTUM_SERVICE_API_URL
+env_vars=$(env | grep "^.*TUTUM_API_URL" | cut -d= -f1 | tr '\n' ' ')
+for env_var in $env_vars
+do
+  curl -H "Authorization: $TUTUM_AUTH" -H "Accept: application/json" $env_var
+done
+  
  
