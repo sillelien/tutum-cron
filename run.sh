3e812336a0071eb769e61ed7feac2c72a51d65f5
neilellis <hello@neilellis.me>
2015-07-07 23:26:20 +0100

    Bug fixing

--- run.sh
+++ run.sh
@@ -27,7 +27,7 @@ do
       service_url=${!env_var}
 
 cat <<EOF >> /tmp/cron.tmp
-${schedule} curl -X POST -H "Authorization: $TUTUM_AUTH" -H "Accept: application/json" ${service_url}start/"
+${schedule} curl -X POST -H "Authorization: $TUTUM_AUTH" -H "Accept: application/json" ${service_url}start/
 EOF
     done
 
