0afd95f34ed8dae41447bb37ae459c9363aa74df
neilellis <hello@neilellis.me>
2015-07-07 18:01:08 +0100

    Bug fixing

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -x
 
-env_vars=$(env | grep "^.*TUTUM_API_URL" | cut -d= -f1 | tr '\n' ' ')
+env_vars=$(env | grep ".*_TUTUM_API_URL=" | cut -d= -f1 | tr '\n' ' ')
 for env_var in $env_vars
 do
   curl -H "Authorization: $TUTUM_AUTH" -H "Accept: application/json" $env_var
