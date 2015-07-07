9b115166e856e857a477e0778a842bb8cc7c7cd7
neilellis <hello@neilellis.me>
2015-07-07 22:20:53 +0100

    Bug fixing

--- run.sh
+++ run.sh
@@ -1,27 +1,34 @@
 #!/usr/bin/env bash
 set -x
 
-env_vars=$(env | grep ".*_TUTUM_API_URL=" | cut -d= -f1 | tr '\n' ' ')
-for env_var in $env_vars
+while true
 do
-  # Set on the remote service
-  schedule_env_var=${env_var%_TUTUM_API_URL}_ENV_CRON_SCHEDULE
-  #  Set on the cron service
-  schedule_var=${env_var%_TUTUM_API_URL}_CRON_SCHEDULE
+    env_vars=$(env | grep ".*_TUTUM_API_URL=" | cut -d= -f1 | tr '\n' ' ')
+    for env_var in $env_vars
+    do
+      # Set on the remote service
+      schedule_env_var=${env_var%_TUTUM_API_URL}_ENV_CRON_SCHEDULE
+      #  Set on the cron service
+      schedule_var=${env_var%_TUTUM_API_URL}_CRON_SCHEDULE
 
-  if [[ -n $schedule_var ]]
-  then
-    schedule=${!schedule_var}
-  else
-    schedule=${!schedule_env_var}
-  fi
-  echo ${schedule} "curl -X POST -H \"Authorization: $TUTUM_AUTH\" -H \"Accept: application/json\" ${!env_var}start/ &> /dev/stdout" > /tmp/cron.tmp
+      if [[ -n $schedule_var ]]
+      then
+        schedule="${!schedule_var}"
+      else
+        schedule="${!schedule_env_var}"
+      fi
+      rm /tmp/cron.tmp
+      service_url=${!env_var}
+cat <<EOF >> /tmp/cron.tmp
+${schedule} curl -X POST -H "Authorization: $TUTUM_AUTH\" -H "Accept: application/json" ${service_url}start/ &> /dev/stdout"
+EOF
+    done
+
+    echo "Installing new crontab"
+    cat /tmp/cron.tmp
+
+    crontab /tmp/cron.tmp
+    sleep 300
 done
-
-echo "Installing new crontab"
-cat /tmp/cron.tmp
-
-crontab /tmp/cron.tmp
-sleep infinity
   
  
