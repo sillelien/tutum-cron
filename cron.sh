159b60acfec0aa3c3fda952ddeefa8cde17c09f8
neilellis <hello@neilellis.me>
2015-07-07 23:20:02 +0100

    Bug fixing

--- cron.sh
+++ cron.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "root" > /etc/fcron/fcron.allow
 #chmod 644 /etc/fcron/
-#chmod 644 /etc/fcron/fcron.{conf,allow,deny}
+chmod 644 /etc/fcron/fcron.conf /etc/fcron/fcron.allow /etc/fcron/fcron.deny
 [ ! -f /run/fcron.pid ] || rm /run/fcron.pid
 exec /usr/sbin/fcron --foreground
